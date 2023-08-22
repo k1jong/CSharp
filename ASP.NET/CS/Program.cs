dd7fefd90faa8f5f0abea6a977062d29b894043f
k1jong <105771489+k1jong@users.noreply.github.com>
2023-08-22 16:55:52 +0900

    Delete Program.cs

--- ASP.NET/CS/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
-
-app.MapControllerRoute(name: "{default}", pattern: "{controller=home}/{action=index}/id?");
-
-app.Run();
