638ae5a06ff9061ef140f3c9ee9053a55d16d367
k1jong <105771489+k1jong@users.noreply.github.com>
2023-08-25 10:52:11 +0900

    Add files via upload
    			////1. TempData["data1"] = obj;
    			////2. ViewBag.Employee = obj;
    			////3. ViewData["data1"] = obj;
    			////4. Stronly Type

---
+++ ASP.NET/MVC_Typed/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Typed.Models;
+using System.Diagnostics;
+
+namespace MVC_Typed.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			//EmployModel obj = new EmployModel
+			//{
+			//	EmpID = 100,
+			//	EmpName = "홍길동",
+			//	Designation = "대리",
+			//	Salary = 25000
+			//};
+
+			////1. TempData["data1"] = obj;
+			////2. ViewBag.Employee = obj;
+			////3. ViewData["data1"] = obj;
+			////4. Stronly Type
+
+			//return View(obj);
+
+			var myEmployees = new List<EmployModel>
+			{
+				new EmployModel {EmpID = 100,
+				EmpName = "홍길동",
+				Designation = "대리",
+				Salary = 25000},
+                new EmployModel {EmpID = 200,
+                EmpName = "이순신",
+                Designation = "부장",
+                Salary = 50000},
+                new EmployModel {EmpID = 300,
+                EmpName = "강감찬",
+                Designation = "상무",
+                Salary = 75000},
+            };
+
+			return View(myEmployees);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ ASP.NET/MVC_Typed/Models/EmployModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Typed.Models
+{
+	public class EmployModel
+	{
+		public int EmpID { get; set; }
+		public string EmpName { get; set; }
+		public string Designation { get; set; }
+		public int Salary { get; set; }
+	}
+}
