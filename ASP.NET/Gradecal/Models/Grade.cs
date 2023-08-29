9e14cb2643230589a9cb4343e448e7a2ef6e2193
k1jong <105771489+k1jong@users.noreply.github.com>
2023-08-29 15:18:37 +0900

    Add files via upload

---
+++ ASP.NET/Gradecal/Models/Grade.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator.Models
+{
+    public class Grade
+    {
+        public GradeType Kor { get; set; }
+        public GradeType Math { get; set; }
+        public GradeType Eng { get; set; }
+        public GradeType Sci { get; set; }
+        public GradeType His { get; set; }
+    }
+    public enum GradeType
+    {
+        A=95, B=85, C=75, D=65 , A_Plus=100, B_Plus=90, C_Plus=80, D_plus=70
+    }
+}
