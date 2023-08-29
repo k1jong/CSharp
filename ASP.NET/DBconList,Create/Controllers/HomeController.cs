f53c5dcaf44234093147ee7bd643c68eff8d8dc9
k1jong <105771489+k1jong@users.noreply.github.com>
2023-08-29 17:42:11 +0900

    Add files via upload

---
+++ ASP.NET/DBconList,Create/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD.Models;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StudentDbContext studentDb;
+        public HomeController(StudentDbContext studentDb)
+        {
+            this.studentDb = studentDb;
+        }
+        public IActionResult Index()
+        {
+
+            var stdData = studentDb.Student.ToList();
+            return View(stdData);
+        }
+        //Get화면
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: 삽입기능
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Hp")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                studentDb.Add(student);
+                await studentDb.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET/DBconList,Create/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace MVC_CRUD.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)  //초기화 변수
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        //public static implicit operator StreamContent(StudentDbContext v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
